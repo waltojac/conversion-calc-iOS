9f7c72f0d1b3256eeb008483df68b7446b35497a
Jacob Walton <waltojac@mail.gvsu.edu>
2018-09-23 15:59:41 -0400

    Project completed (we think).

---
+++ Conversion Calculator/SettingsViewController.swift
@@ -0,0 +1,127 @@
+//
+//  SettingsViewController.swift
+//  Conversion Calculator
+//
+//  Created by Jacob Walton on 9/21/18.
+//  Copyright © 2018 Jake Walton. All rights reserved.
+//
+
+import UIKit
+
+protocol SettingsViewControllerDelegate {
+    func unitPickerSelection(toUnit: String, fromUnit: String)
+}
+
+class SettingsViewController: UIViewController {
+   
+    @IBOutlet weak var fromUnitLabel: UILabel!
+    
+    @IBOutlet weak var toUnitLabel: UILabel!
+    
+    var selectedLabel: String = "from"
+    var lengthMode: Bool?
+    
+    @IBOutlet weak var picker: UIPickerView!
+    var pickerData: [String] = [String]()
+    var choice: String = ""             //Maybe need to fill
+    var delegate: SettingsViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        if (lengthMode == true){
+            for each in LengthUnit.allCases{
+                self.pickerData.append(each.rawValue)
+            }
+        }
+        else {
+            for each in VolumeUnit.allCases{
+                self.pickerData.append(each.rawValue)
+            }
+        }
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        self.fromUnitLabel.text = pickerData[1]
+        self.toUnitLabel.text = pickerData[0]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func transferUnits(toUnit: String, fromUnit: String) {
+        toUnitLabel.text = toUnit
+        fromUnitLabel.text = fromUnit
+    }
+
+    // MARK: - Navigation
+    
+
+    @IBAction func fromUnitTapped(_ sender: UITapGestureRecognizer) {
+        selectedLabel = "from"
+        picker.isHidden = false;
+    }
+    
+    @IBAction func toUnitTapped(_ sender: UITapGestureRecognizer) {
+        selectedLabel = "to"
+        picker.isHidden = false;
+    }
+    
+    @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func save(_ sender: UIBarButtonItem) {
+        if let d = self.delegate {
+            d.unitPickerSelection(toUnit: toUnitLabel.text!, fromUnit: fromUnitLabel.text!)
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>,
+                               with event: UIEvent?) {
+        if (selectedLabel == "from"){
+            fromUnitLabel.text = self.choice
+        }
+        else{
+            toUnitLabel.text = self.choice
+        }
+        picker.isHidden = true
+        
+    }
+    
+    
+}
+
+extension SettingsViewController : UIPickerViewDataSource, UIPickerViewDelegate {
+    // The number of columns of data
+    func numberOfComponents(in: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return self.pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        self.choice = self.pickerData[row]
+    }
+}
--- Conversion Calculator/ViewController.swift
+++ Conversion Calculator/ViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SettingsViewControllerDelegate {
+    @IBOutlet weak var toTextField: DecimalMinusTextField!
+    @IBOutlet weak var fromTextField: DecimalMinusTextField!
+    @IBOutlet weak var fromUnitLabel: UILabel!
+    @IBOutlet weak var toUnitLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var lengthMode: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,81 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func unitPickerSelection(toUnit: String, fromUnit: String){
+        toUnitLabel.text = toUnit
+        fromUnitLabel.text = fromUnit
+    }
+    
+    @IBAction func clearPressed(_ sender: UIButton) {
+        toTextField.text = ""
+        fromTextField.text = ""
+    }
+    @IBAction func fromTapped(_ sender: DecimalMinusTextField) {
+        toTextField.text = ""
+    }
+    @IBAction func toTapped(_ sender: DecimalMinusTextField) {
+        fromTextField.text = ""
+    }
+    
+    @IBAction func modeTapped(_ sender: UIButton) {
+        if (lengthMode){
+            lengthMode = false
+            fromUnitLabel.text = VolumeUnit.Liters.rawValue
+            toUnitLabel.text = VolumeUnit.Gallons.rawValue
+            titleLabel.text = "Volume Conversion Calculator"
+        }
+        else {
+            lengthMode = true
+            fromUnitLabel.text = LengthUnit.Yards.rawValue
+            toUnitLabel.text = LengthUnit.Meters.rawValue
+            titleLabel.text = "Length Conversion Calculator"
+        }
+        toTextField.text = ""
+        fromTextField.text = ""
+    }
+    
+    
+    @IBAction func calculate(_ sender: UIButton) {
+        //for length mode
+        if (lengthMode){
+            if (fromTextField.text != ""){
+                let toEnum = LengthUnit(rawValue: toUnitLabel.text!)!
+                let fromEnum = LengthUnit(rawValue: fromUnitLabel.text!)!
+                let convKey =  LengthConversionKey(toUnits: toEnum, fromUnits: fromEnum)
+                toTextField.text = String(Double(fromTextField.text!)! * lengthConversionTable[convKey]!);
+            }
+             else if (toTextField.text != ""){
+                let toEnum = LengthUnit(rawValue: fromUnitLabel.text!)!
+                let fromEnum = LengthUnit(rawValue: toUnitLabel.text!)!
+                let convKey =  LengthConversionKey(toUnits: toEnum, fromUnits: fromEnum)
+                fromTextField.text = String(Double(toTextField.text!)! * lengthConversionTable[convKey]!);
+            }
+        }
+            
+        // for volume mode
+        else {
+            if (fromTextField.text != ""){
+                let toEnum = VolumeUnit(rawValue: toUnitLabel.text!)!
+                let fromEnum = VolumeUnit(rawValue: fromUnitLabel.text!)!
+                let convKey =  VolumeConversionKey(toUnits: toEnum, fromUnits: fromEnum)
+                toTextField.text = String(Double(fromTextField.text!)! * volumeConversionTable[convKey]!);
+            }
+            else if (toTextField.text != ""){
+                let toEnum = VolumeUnit(rawValue: fromUnitLabel.text!)!
+                let fromEnum = VolumeUnit(rawValue: toUnitLabel.text!)!
+                let convKey =  VolumeConversionKey(toUnits: toEnum, fromUnits: fromEnum)
+                fromTextField.text = String(Double(toTextField.text!)! * volumeConversionTable[convKey]!);
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navVC = segue.destination as? UINavigationController
+        let settingsVC = navVC?.viewControllers.first as! SettingsViewController
+        settingsVC.delegate = self
+        settingsVC.lengthMode = self.lengthMode
+    }
+    
+    
 }
 
